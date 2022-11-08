065d5c0ddfc4dc67329fd643dc9a67e58e316423
Ruslan Yakimov <ruslan87yakimov@gmail.com>
2022-11-08 12:32:36 +0300

    finish

---
+++ script.js
@@ -0,0 +1,36 @@
+const burger = document.querySelector("nav svg");
+burger.addEventListener("click", () => {
+  if (burger.classList.contains("active")) {
+    gsap.to(".links", { x: "100%" });
+    gsap.to(".line", { stroke: "white" });
+    gsap.set("body", { overflowX: "hidden" });
+  } else {
+    gsap.to(".links", { x: "0%" });
+    gsap.to(".line", { stroke: "black" });
+    gsap.fromTo(
+      ".links a",
+      { opacity: 0, y: -20 },
+      { opacity: 1, y: 20, delay: 0.12, stagger: 0.25 }
+    );
+    gsap.set("body", { overflow: "hidden" });
+  }
+  burger.classList.toggle("active");
+});
+
+const videos = gsap.utils.toArray(".video");
+gsap.to(".video", { opacity: 0 });
+videos.forEach((video) => {
+  ScrollTrigger.create({
+    trigger: video,
+    start: "top center",
+    end: "bottom center",
+    markers: true,
+    onEnter: () => {
+      gsap.to(video, { opacity: 1 });
+      video.play();
+    },
+    OnEnterBack: () => video.play(),
+    onLeave: () => video.pause(),
+    onLeaveBack: () => video.pause(),
+  });
+});
